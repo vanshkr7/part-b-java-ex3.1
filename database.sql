251aec39bfdd11fec2b430db9de2be7cac2153bf
vanshkr7 <vanshkumar6200@gmail.com>
2025-10-26 20:59:44 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
+
+INSERT INTO Employee VALUES (1, 'Vansh', 45000.00), (2, 'Rohan', 55000.00), (3, 'Amit', 60000.00);
